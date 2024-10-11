4d44c45beaf48fac1f3d75ae670462408094ec18
Zeshan ali <mr.zeshan934@gmail.com>
2024-10-12 02:09:29 +0500

    Create variables.js
    variables are learned

---
+++ 01_basic/variables.js
@@ -0,0 +1,15 @@
+const consumerId = 123
+let consumerName = "John"
+var consumerCity = "New York"
+consumerPass = 123456
+
+/*
+value of const can not be changed
+value of let can be changed
+value of var can be changed
+
+we ll be using let in the future
+
+*/ 
+
+console.table([consumerId, consumerName, consumerCity, consumerPass])
